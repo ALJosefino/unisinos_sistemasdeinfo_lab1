125e02d3369fd948ba36796158e46cc667d53020
André L Josefino <63529038+ALJosefino@users.noreply.github.com>
2023-12-10 23:41:45 -0300

    Questão
    prova colocada em prática

---
+++ 09 - 17grau_b/Gb005/src/gb005/Gb005.java
@@ -0,0 +1,24 @@
+
+package gb005;
+                
+public class Gb005 {
+
+    public static void main(String[] args) {
+    
+    public boolean metodo(){ 
+        
+            if( x > 0 && y > 0 ){ 
+         
+            return true; 
+    
+            } else { 
+                
+            return false; 
+            
+            } 
+            
+        }
+    
+    }
+    
+}
